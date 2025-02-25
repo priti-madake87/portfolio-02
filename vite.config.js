eed9e1d27f5299b3259892df2e827f40f169adb5
priti-madake87 <pritimadake929@gmail.com>
2025-02-24 16:40:42 -0800

    Deploy React app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/portfolio-02/',  // 👈 Set this to match your GitHub repository name
+});
